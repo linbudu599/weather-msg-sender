84e8279afc275439394f16ed9f2d72245fbe1826
Linbudu <48507806+linbudu599@users.noreply.github.com>
2021-06-26 10:33:56 +0800

    ci_debug_check_01

--- src/helper.ts
+++ src/helper.ts
@@ -92,7 +92,9 @@ export async function sendMsg(content: IMsgProps) {
 
   const phone = process.env.USE_SELF!
     ? [process.env.SELF_PHONE!]
-    : [process.env.SELF_PHONE!, process.env.TARGET_PHONE!];
+    : [process.env.SELF_PHONE!];
+
+  console.log("phone: ", phone);
 
   client.SendSms(
     {
